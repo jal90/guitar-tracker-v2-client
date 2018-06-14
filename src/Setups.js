ca48cf2123d6121495b22dec7d9519bcc96b9e69
jal90 <mezzyjezze@gmail.com>
2018-06-14 12:36:16 -0400

    Commit setups before refactoring AJAX requests
    Commented out buggy setups code because I'm going to try refactoring
    the whole app so all AJAX functions exist in one file rather than a
    bunch in the App component and others in separate components.
    
    In conjunction with the store that can save user token and even guitar
    and setup ids, this should be a worthwhile refactoring.

--- src/Setups.js
+++ src/Setups.js
@@ -3,20 +3,30 @@ import React, {Component} from 'react';
 class Setups extends Component {
 
   render() {
-    const userHasSetups = this.props.setups.length === undefined ? false : true
+    const userHasSetups = this.props.setups.length === undefined
+      ? false
+      : true
     let setups
 
-    if (userHasSetups) {
-    setups = this.props.setups.map(setup => <div className="column is-one-quarter" key={setup.id}>
-      <p className="setup-id">{setup.id}</p>
-      <p className="guitar-id">{setup.guitar_id}</p>
-      <p className="string-gauge">{setup.string_gauge}</p>
-      <p className="string-brand">{setup.string_brand}</p>
-      <p className="date-strings-changed">{setup.date_strings_changed}</p>
-      <p className="date-of-setup">{setup.date_of_setup}</p>
-      <p className="setup-notes">{setup.setup_notes}</p>
-    </div>)
-  }
+    // if (userHasSetups) {
+    //   setups = this.props.setups
+    //   if (setups.guitar_id === this.props.guitar.id) {
+    //
+    //   setups.map(setup => {
+    //
+    //       <div key={setup.id}>
+    //         <p className="setup-id">Id: {setup.id}</p>
+    //         <p className="guitar-id">Guitar_id: {setup.guitar_id}</p>
+    //         <p className="string-gauge">String gauge: {setup.string_gauge}</p>
+    //         <p className="string-brand">String brand: {setup.string_brand}</p>
+    //         <p className="date-strings-changed">Date strings changed: {setup.date_strings_changed}</p>
+    //         <p className="date-of-setup">Date of setup: {setup.date_of_setup}</p>
+    //         <p className="setup-notes">Setup notes: {setup.setup_notes}</p>
+    //       </div>
+    //         })
+    //     }
+    //
+    // }
 
     return <div>{setups}</div>
   }
