32e805959fc4b14bf3444d2b33b2ffded12e3682
jal90 <mezzyjezze@gmail.com>
2018-06-22 19:06:10 -0400

    Fix isLoggedIn logic to trigger on successful API
    App's state isLoggedIn is now set to true or false on successful signIn
    and signOut calls, respectively.
    Use ternary statement in App.js to render components, as opposed to
    having that logic decided in each individual component.
    
    Cleaned up some comments

--- src/App.js
+++ src/App.js
@@ -9,23 +9,16 @@ import { signUpCall, signInCall, signOutCall, getGuitarsCall } from './api.js'
 
 import 'bulma/css/bulma.css'
 import $ from 'jquery';
-const store = require('./store.js')
+import store from './store.js'
 
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      // TODO: move guitars and setups out of here and into components.
-      guitars: [],
-      setups: [],
-      isLoggedIn: false
-    }
+    this.state = {isLoggedIn: false}
 
-    this.getGuitars = this.getGuitars.bind(this)
-    this.signUserIn = this.signUserIn.bind(this)
-    this.logUserOut = this.logUserOut.bind(this)
-    // this.getSetups = this.getSetups.bind(this)
+    this.signIn = this.signIn.bind(this)
+    this.signOut = this.signOut.bind(this)
   }
 
   signUp (email, password) {
@@ -45,73 +38,42 @@ class App extends Component {
     const credentials = {
       credentials:{
         email: email,
-        password: password      }
+        password: password
+      }
     }
     // API call from api.js
     signInCall(credentials)
-    // TODO: reconfigure the `isLoggedIn` logic
       .then(res => {
         store.token = res.user.token
-        // this.setState({isLoggedIn: true})
+        this.setState({isLoggedIn: true})
       })
-      // TODO: if getGuitars is moved to Guitars component, import this function from there.
-      .then(this.getGuitars)
       // TODO: write error catching
   }
 
   signOut () {
     // API call from api.js
     signOutCall()
-    // TODO: then and catch
-
-          // .then(() => {
-          //   store.token = ''
-          // })
+      .then(this.setState({isLoggedIn: false}))
+      .then(store.token = '')
+    // TODO: catch error
   }
 
-
-// TODO: move this into guitars component?
-  getGuitars() {
-    // API call from api.js
-    getGuitarsCall()
-      .then(res => {
-        const guitars = res
-        this.setState({guitars})
-      })
-   }
-
-   // getSetups() {
-   //   $.ajax({
-   //     url: 'http://localhost:4741/setups',
-   //     method: 'GET',
-   //     headers: {
-   //       contentType: 'application/json',
-   //       Authorization: 'Token token=' + store.token
-   //     }
-   //   })
-   //     .then(res => {
-   //       const setups = res
-   //       this.setState({setups})
-   //     })
-   //  }
-
-// TODO: change this to an on success API call in the sign in function, or rework another way
-   signUserIn() {
-     this.setState({isLoggedIn: true})
-   }
-
-   logUserOut() {
-     this.setState({isLoggedIn: false})
-   }
-
   render() {
+    const { isLoggedIn } = this.state
 
     return (
-      <div>
-        <SigninForm getGuitars={this.getGuitars} getSetups={this.getSetups} signInAction={this.signIn} signUserIn={this.signUserIn} isLoggedIn={this.state.isLoggedIn} />
-        <SignupForm signUpAction={this.signUp}/>
-        <SignoutButton signOutAction={this.signOut} isLoggedIn={this.state.isLoggedIn} />
-        <Guitars getGuitarsAction={this.getGuitars} guitars={this.state.guitars} setups={this.state.setups} isLoggedIn={this.state.isLoggedIn} />
+      <div className="App">
+        { isLoggedIn ?
+          <div>
+          <SignoutButton signOutAction={this.signOut} />
+          <Guitars />
+        </div>
+          :
+          <div>
+          <SigninForm signInAction={this.signIn} />
+          <SignupForm signUpAction={this.signUp} />
+        </div>
+        }
       </div>
     );
   }
