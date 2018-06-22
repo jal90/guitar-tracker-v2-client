b9678b91f6f3cc88eb6c77145a9276d8104369d1
jal90 <mezzyjezze@gmail.com>
2018-06-22 19:04:06 -0400

    Add getGuitars logic to guitars component
    Move getGuitars logic out of App.js and into Guitars.js
    Use componentDidMount to trigger getGuitars, in conjunction with guitars
    now only being rendered by App when user is signed in, getGuitars is
    triggered when user signs in.

--- src/Guitars.js
+++ src/Guitars.js
@@ -4,18 +4,29 @@ import DeleteGuitar from './DeleteGuitar'
 import React, {Component} from 'react';
 import Setups from './Setups'
 import './Guitars.css'
+import { getGuitarsCall } from './api.js'
 
 class Guitars extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {guitars: []}
+  }
+
+  componentDidMount () {
+    // API call from api.js
+    getGuitarsCall()
+      .then(res => {
+        this.setState({guitars: res})
+      })
+   }
 
   render() {
-    const userHasGuitars = this.props.guitars.length === undefined ? false : true
+    const userHasGuitars = this.state.guitars.length === undefined ? false : true
     let catalog
-    let isLoggedIn = this.props.isLoggedIn
 
-    if (isLoggedIn) {
-    if (userHasGuitars) {
+    { userHasGuitars ?
       catalog =
-      this.props.guitars.map(guitar => <div className="column is-one-quarter" key={guitar.id}>
+      this.state.guitars.map(guitar => <div className="column is-one-quarter" key={guitar.id}>
         <div className="card">
           <div className="card-content">
             <p className="title">{guitar.make}</p>
@@ -23,19 +34,20 @@ class Guitars extends Component {
             <p className="year">{guitar.year}</p>
             <p className="price">{guitar.price}</p>
 
+            {/* TODO: pass appropriate function to components */}
             <UpdateGuitar guitar={guitar} getGuitars={this.props.getGuitars}/>
             <DeleteGuitar id={guitar.id} getGuitars={this.props.getGuitars}/>
-            <Setups guitar={guitar} setups={this.props.setups} />
+            {/* <Setups guitar={guitar} setups={this.props.setups} /> */}
           </div>
         </div>
       </div>)
-    } else {
+      :
       catalog = <div className="column">Create a guitar with the form that doesn't exist yet!</div>
     }
-  }
 
 
     return (<div>
+      {/* TODO: pass appropriate funciton to CreateGuitar */}
       <CreateGuitar getGuitars={this.props.getGuitars}/>
 
       <div className="container">
