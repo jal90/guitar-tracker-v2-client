cfddb9c0ad45da893863392e3d65cd6d05576714
jal90 <mezzyjezze@gmail.com>
2018-07-10 11:40:37 -0400

    Moved hide/show logic out and up into App
    App now knows if a user is signed in or not, so this logic doesn't
    need to be handled by the signin form component

--- src/signin-form.js
+++ src/signin-form.js
@@ -29,22 +29,14 @@ class SigninForm extends Component {
   }
 
   render() {
-    // let isLoggedIn = this.props.isLoggedIn
-    let form
-
-    // if (!isLoggedIn) {
-      form =
-        <form onSubmit={this.handleSignIn}>
-        <label>Sign in</label>
-        <input placeholder="Email" type="text" value={this.state.email} onChange={this.handleEmailChange} />
-        <input placeholder="Password" type="password" value={this.state.password} onChange={this.handlePasswordChange} />
-        <button type="submit">Submit</button>
-      </form>
-    // }
-
     return (
       <div>
-        {form}
+        <form onSubmit={this.handleSignIn}>
+          <label>Sign in</label>
+          <input placeholder="Email" type="text" value={this.state.email} onChange={this.handleEmailChange} />
+          <input placeholder="Password" type="password" value={this.state.password} onChange={this.handlePasswordChange} />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
