e5aa041e6302ff44c791275843095874d1ed1afd
jal90 <mezzyjezze@gmail.com>
2018-07-10 13:10:50 -0400

    Change to show most recent setup by default
    Each guitar will now have its most recent setup on display,
    and a button will show the entire guitar's history. This will be
    refactored for better formatting.
    
    <CreateSetup /> is imported so users can create a setup right in the
    guitar view.

--- src/Setups.js
+++ src/Setups.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import CreateSetup from './CreateSetup'
 import {getSetupsCall, createSetupCall} from './api.js'
 
 class Setups extends Component {
@@ -14,7 +15,9 @@ class Setups extends Component {
   }
 
   toggleSetupsView() {
-    this.setState({isVisible: !this.state.isVisible})
+    this.setState({
+      isVisible: !this.state.isVisible
+    })
   }
 
   // TODO: a new setupCreateCall needs to be called every time a user updates their guitar (and just after creation as well). To the user experience, creating the guitars first setup should seem
@@ -48,47 +51,54 @@ class Setups extends Component {
   render() {
     let setups = this.state.setups.filter(setup => setup.guitar_id === this.props.guitarId)
 
-      const newestSetup = setups[0]
-
+    const newestSetup = setups[0]
 
     const userHasSetups = setups.length === 0
       ? false
       : true
 
     return (<div>
-      <button onClick={this.createSetupAction}>Create setup for guitar</button>
+      <CreateSetup guitarId={this.props.guitarId} />
 
-      <button onClick={this.toggleSetupsView}>{this.state.isVisible ? 'hide history' : 'See all setups'}</button>
+      <button onClick={this.toggleSetupsView}>{
+          this.state.isVisible
+            ? 'hide history'
+            : 'See all setups'
+        }</button>
 
       {/* This is one way to show the newest setup... */}
-      <div>{newestSetup === undefined ? '' :
-      <div>  <p>{newestSetup.string_brand}</p>
-        <p>{newestSetup.string_gauge}</p> </div>
-      }</div>
-      {userHasSetups
+      <div>{
+          newestSetup === undefined
+            ? ''
+            : <div>
+                <p>string brand: {newestSetup.string_brand}</p>
+                <p>gauge: {newestSetup.string_gauge}</p>
+                <p>string change date {newestSetup.date_strings_changed}</p>
+                <p>date of setup {newestSetup.date_of_setup}</p>
+                <p>setup notes{newestSetup.setup_notes}</p>
+              </div>
+        }</div>
+      {
+        userHasSetups
         // TODO: Show only most recent setup on home page (use created_at/ updated_at properties to do this). Only Map over all guitar's setups when user wants to see a history
-        ?
+          ? setups.map(setup => <div key={setup.id} className={this.state.isVisible
+              ? 'visible'
+              : 'invisible'}>
+            {/* <p className="setup-id">Id: {setup.id}</p> */}
+            {/* <p className="guitar-id">Guitar_id: {setup.guitar_id}</p> */}
+            <p className="string-gauge">String gauge: {setup.string_gauge}</p>
+            <p className="string-brand">String brand: {setup.string_brand}</p>
+            <p className="date-strings-changed">Date strings changed: {setup.date_strings_changed}</p>
+            <p className="date-of-setup">Date of setup: {setup.date_of_setup}</p>
+            <p className="setup-notes">Setup notes: {setup.setup_notes}</p>
+            <h4 className="created-at">created at: {setup.created_at}</h4>
 
+          </div>)
 
-        setups.map(setup =>
-            <div key={setup.id} className={this.state.isVisible ? 'visible' : 'invisible'}>
-              {/* <p className="setup-id">Id: {setup.id}</p> */}
-              {/* <p className="guitar-id">Guitar_id: {setup.guitar_id}</p> */}
-              <p className="string-gauge">String gauge: {setup.string_gauge}</p>
-              <p className="string-brand">String brand: {setup.string_brand}</p>
-              <p className="date-strings-changed">Date strings changed: {setup.date_strings_changed}</p>
-              <p className="date-of-setup">Date of setup: {setup.date_of_setup}</p>
-              <p className="setup-notes">Setup notes: {setup.setup_notes}</p>
-              <h4 className="created-at">created at: {setup.created_at}</h4>
-
-            </div>
-        )
-
-        :
-        <div>No setups on this guitar</div>
+          : <div>No setups on this guitar</div>
       }
 
-    </div>)
+      </div>)
   }
 }
 
